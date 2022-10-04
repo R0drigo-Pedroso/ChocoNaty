e6df3355661760f084e109c62eaabb57fe2da3a9
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-10-04 10:46:03 -0300

    atualização do php

---
+++ src/banco.php
@@ -0,0 +1,30 @@
+<?php
+namespace ChocoNaty;
+use PDO, Exception;
+
+abstract class Banco {
+    private static string $servidor = "localhost";
+    private static string $usuario = "suniow89_rodrigo";
+    private static string $senha = "Rp582700+";
+    private static string $banco = "suniow89_microblog_rodrigo";
+    private static PDO $conexao; 
+
+    public static function conecta():PDO {
+        try {
+            self::$conexao = new PDO(
+                "mysql:host=".self::$servidor."; 
+                dbname=".self::$banco.";
+                charset=utf8",
+                self::$usuario, 
+                self::$senha
+            );
+            self::$conexao->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            // echo "Ok!"; // teste
+        } catch (Exception $erro) {
+            die("Deu ruim: ".$erro->getMessage());
+        }
+        return self::$conexao;
+    }
+}
+// Banco::conecta(); // teste
